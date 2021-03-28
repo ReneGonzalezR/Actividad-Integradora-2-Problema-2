a9cd5dc8f2bb51096bd25589ee6c342c7e0ad5f7
Rene Gonzalez <rene_glez1992@hotmail.com>
2021-03-28 14:41:15 -0700

    Concluido

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,164 @@ namespace Actividad_Integradora_2_Problema_2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtAntiguedad = new System.Windows.Forms.TextBox();
+            this.lblMenor = new System.Windows.Forms.Label();
+            this.lblEntre = new System.Windows.Forms.Label();
+            this.lblMayor = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(537, 334);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(188, 58);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(124, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 41);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(124, 258);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 41);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Antiguedad: ";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(311, 334);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(188, 58);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(311, 167);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(250, 47);
+            this.txtNombre.TabIndex = 4;
+            // 
+            // txtAntiguedad
+            // 
+            this.txtAntiguedad.Location = new System.Drawing.Point(311, 258);
+            this.txtAntiguedad.Name = "txtAntiguedad";
+            this.txtAntiguedad.Size = new System.Drawing.Size(250, 47);
+            this.txtAntiguedad.TabIndex = 5;
+            // 
+            // lblMenor
+            // 
+            this.lblMenor.AutoSize = true;
+            this.lblMenor.Location = new System.Drawing.Point(442, 497);
+            this.lblMenor.Name = "lblMenor";
+            this.lblMenor.Size = new System.Drawing.Size(34, 41);
+            this.lblMenor.TabIndex = 6;
+            this.lblMenor.Text = "0";
+            // 
+            // lblEntre
+            // 
+            this.lblEntre.AutoSize = true;
+            this.lblEntre.Location = new System.Drawing.Point(442, 556);
+            this.lblEntre.Name = "lblEntre";
+            this.lblEntre.Size = new System.Drawing.Size(34, 41);
+            this.lblEntre.TabIndex = 7;
+            this.lblEntre.Text = "0";
+            // 
+            // lblMayor
+            // 
+            this.lblMayor.AutoSize = true;
+            this.lblMayor.Location = new System.Drawing.Point(442, 614);
+            this.lblMayor.Name = "lblMayor";
+            this.lblMayor.Size = new System.Drawing.Size(34, 41);
+            this.lblMayor.TabIndex = 8;
+            this.lblMayor.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(202, 614);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(228, 41);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Mayor a 5 años:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(202, 556);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(204, 41);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "De 1 a 5 años:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(202, 497);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(218, 41);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Menor a 1 año:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1007, 727);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblMayor);
+            this.Controls.Add(this.lblEntre);
+            this.Controls.Add(this.lblMenor);
+            this.Controls.Add(this.txtAntiguedad);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.Text = "Actividad Integradora 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtAntiguedad;
+        private System.Windows.Forms.Label lblMenor;
+        private System.Windows.Forms.Label lblEntre;
+        private System.Windows.Forms.Label lblMayor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,10 +12,49 @@ namespace Actividad_Integradora_2_Problema_2
 {
     public partial class Form1 : Form
     {
+        string[,] Empleados = new string[100,2];
+        int contador = 0;
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Empleados[contador, 0] = txtNombre.Text;
+            Empleados[contador, 1] = txtAntiguedad.Text;
+            contador++;
+            MessageBox.Show("Empleado Registrado");
+            txtNombre.Text = string.Empty;
+            txtAntiguedad.Text = string.Empty;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int menor = 0, entre = 0, mayor = 0;
+            string personasMenor = string.Empty, personasEntre = string.Empty, personasMayor = string.Empty;
+            for (int i = 0; i < contador; i++)
+            {
+                int antiguedad = Convert.ToInt32(Empleados[i, 1]);
+                if (antiguedad <= 1)
+                {
+                    menor++;
+                    personasMenor = string.Format("{0}, {1} ", personasMenor, Empleados[i, 0]);
+                }
+                else if (antiguedad > 1 && antiguedad < 5)
+                {
+                    entre++;
+                    personasEntre = string.Format("{0}, {1} ", personasEntre ,Empleados[i, 0]);
+                }
+                else if (antiguedad >= 5)
+                {
+                    mayor++;
+                    personasMayor = string.Format("{0}, {1} ", personasMayor, Empleados[i, 0]);
+                }
+            }
+            lblMenor.Text = string.Format("{0}  {1}", menor.ToString(), personasMenor);
+            lblEntre.Text = string.Format("{0}  {1}", entre.ToString(), personasEntre);
+            lblMayor.Text = string.Format("{0}  {1}", mayor.ToString(), personasMayor);
+        }
     }
 }
